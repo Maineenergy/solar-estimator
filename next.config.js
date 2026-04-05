3afe3c1468d017e1d11893e5848ebb83563317e5
Maineenergy <sam@energyexperts.me>
2026-04-05 00:40:36 -0400

    Add next.config.js

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'cdn.admin-sr.com' },
+      { protocol: 'https', hostname: 'maps.googleapis.com' },
+    ],
+  },
+}
+
+module.exports = nextConfig
